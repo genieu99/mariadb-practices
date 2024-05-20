a0119b38aa6dd12a0a1bbccef2b8d4b3a0a097de
genieu99 <yujin0419@sookmyung.ac.kr>
2024-05-20 14:48:12 +0900

    class: bookshop #3

--- jdbc-practices/src/test/java/bookshop/dao/BookDaoTest.java
+++ jdbc-practices/src/test/java/bookshop/dao/BookDaoTest.java
@@ -1,18 +1,25 @@
 package bookshop.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import bookshop.vo.AuthorVo;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BookDaoTest {
 	private static int count = 0;
-	private static AuthorDao authorDao = new AuthorDao();
 	
 	private static AuthorVo mockAuthorVo = new AuthorVo();
+	private static BookVo mockBookVo = new BookVo();
+	
+	private static AuthorDao authorDao = new AuthorDao();
 	private static BookDao bookDao = new BookDao();
 	
 	@BeforeAll
@@ -20,26 +27,34 @@ public class BookDaoTest {
 		mockAuthorVo.setName("칼세이건");
 		authorDao.insert(mockAuthorVo);
 		
-		// count = bookDao.findAll().size();
+		count = bookDao.findAll().size();
 	}
 	
 	@Test
+	@Order(1)
 	public void testInsert() {
-		BookVo vo = new BookVo();
-		vo.setTitle("코스모스");
-		vo.setAuthorNo(mockAuthorVo.getNo());
-		bookDao.insert(vo);
+		mockBookVo.setTitle("코스모스");
+		mockBookVo.setAuthorNo(mockAuthorVo.getNo());
+		bookDao.insert(mockBookVo);
 		
-		assertNotNull(vo.getNo());
+		assertNotNull(mockBookVo.getNo());
 	}
 	
 	@Test
+	@Order(2)
 	public void testFindAll() {
 		assertEquals(count + 1, bookDao.findAll().size());
 	}
 	
+	@Test
+	@Order(3)
+	public void testUpdate() {
+		assertEquals(1, bookDao.update(mockBookVo.getNo(), "대여중"));
+	}
+	
 	@AfterAll
 	public static void cleanUp() {
-		// authorDao.deleteByNo(mockAuthorVo.getNo());
+		bookDao.deleteByNo(mockBookVo.getNo());
+		authorDao.deleteByNo(mockAuthorVo.getNo());
 	}
 }
